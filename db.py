27c67f8da975bb08aa8017bc8ce23ec163f69e24
Dev-bot-create <devmail2026@gmail.com>
2026-03-31 20:37:58 +0300

    fix db connection for deployment

--- db.py
+++ db.py
@@ -1,19 +1,5 @@
 import psycopg2
+import os
 
 def get_connection():
-    conn = psycopg2.connect(
-        host="127.0.0.1",
-        database="testdb",
-        user="postgres",
-        password="postgres",
-        port="5433"
-    )
-    return conn
-
-if __name__ == "__main__":
-    try:
-        conn = get_connection()
-        print("Connected to PostgreSQL successfully!")
-        conn.close()
-    except Exception as e:
-        print("Error:", e)
+    return psycopg2.connect(os.environ.get("DATABASE_URL"))
